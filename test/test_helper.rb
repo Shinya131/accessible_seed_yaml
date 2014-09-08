4b82de9a0bedf8b6dbc45a2ae89652439e433e1f
Shinya131 <nagai3mt5b@gmail.com>
2014-09-08 15:12:53 +0900

    seed split by record

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,11 +8,25 @@ end
 
 # test data
 module TestData
-  SeedString = 
-    "data1:" +
-    "  id: 1" +
-    "  name: \"coffee\"" +
-    "data2:" +
-    "  id: 2" +
-    "  name: \"tea\""
+  SeedStringRecord01 =
+    "data1:\n" +
+    "  id: 1\n" +
+    "  name: \"coffee\"\n"
+  
+  SeedStringRecord02 = 
+    "data2:\n" +
+    "  id: 2\n" +
+    "  name: \"tea\"\n"
+  
+  SeedStringRecord03 = 
+    "data3:\n" +
+    "  id: 3\n" +
+    "  name: \"water\"\n"
+    
+  SeedString =
+    [
+      SeedStringRecord01,
+      SeedStringRecord02,
+      SeedStringRecord03
+    ].join
 end
--- test/test_seed.rb
+++ test/test_seed.rb
@@ -10,4 +10,10 @@ class TestSeed < MiniTest::Unit::TestCase
   def test_seed
     assert_equal(TestData::SeedString, @seed.seed)
   end
+  
+  def test_records
+    assert_equal(TestData::SeedStringRecord01, @seed.split_by_record[0])
+    assert_equal(TestData::SeedStringRecord02, @seed.split_by_record[1])
+    assert_equal(TestData::SeedStringRecord03, @seed.split_by_record[2])
+  end
 end
